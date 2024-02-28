b6c2971849a9ba1282327a7641321e090762a299
Anton Marini <vade@vade.info>
2024-02-27 19:23:38 -0500

    Time range visibility for transitions

--- Sources/OpenTimelineIO-AVFoundation/OpenTimelineIO-Extensions/Clip.swift
+++ Sources/OpenTimelineIO-AVFoundation/OpenTimelineIO-Extensions/Clip.swift
@@ -17,14 +17,19 @@ extension Clip
         guard
             let externalReference = self.mediaReference as? ExternalReference,
             let asset = externalReference.toAVAsset(baseURL: baseURL),
-            var timeRangeInAsset = self.sourceRange?.toCMTimeRange()
+            let parent = self.parent as? Item
         else
         {
             return nil
         }
-
-//        let rangeInParent = try self.rangeInParent().toCMTimeRange()
-        let rangeInParent = try self.visibleRange().toCMTimeRange()
+        
+        // This accounts for visible ranges which also account for transition times
+        var timeRangeInAsset = try self.visibleRange().toCMTimeRange()
+        let rangeInParent = try self.transformed(timeRange: self.visibleRange(), toItem:parent ).toCMTimeRange()
+        
+        // if we dont want this, we would rather do:
+        // var timeRangeInAsset = self.sourceRange?.toCMTimeRange()
+        // let rangeInParent = try self.rangeInParent().toCMTimeRange()
 
         // if we have timecode from our asset
         do
